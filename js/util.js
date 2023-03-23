e15721bc8a22ff1d2ca23f921ffba440f4e15e92
Your Name <you@example.com>
2023-03-23 10:30:34 +0300

    добавил массивы со случайными данными

--- js/util.js
+++ js/util.js
@@ -1,37 +1,24 @@
+/* eslint-disable no-console */
+
+
 // Функция, возвращающая случайное целое число из переданного диапазона включительно (min, max)
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 getRandomNumber(1, 33);
-
 
 
 // Функция для проверки максимальной длины строки
 const checkingString = (textString, countString) => {
   return textString.length <= countString;
 };
-
 checkingString ('Проверочная строка ', 50);
 
-const names = [
-  'Дима',
-  'Тимофей',
-  'Алексей',
-  'Мария',
-  'Кристина',
-  'Вика',
-  'Игорь',
-  'Виктор',
-  'Катя',
-  'Денис',
-];
 
 // Случайный элемент массива
 const getRandomElementArray = (array) => {
-  return names[getRandomNumber(0, array.length - 1)];
+  return [getRandomNumber(0, array.length - 1)];
 };
-
-console.log(getRandomElementArray(names));
+getRandomElementArray();
 
 //export { getRandomNumber, checkingString, getRandomElementArray };
